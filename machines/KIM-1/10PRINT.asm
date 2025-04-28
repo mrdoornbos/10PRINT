ebaf01cd359d01c42b5cdb9b9ba4d889634aa3ea
Michael Doornbos <mike@imapenguin.com>
2025-04-28 15:31:38 -0400

    use timer for seed

--- machines/KIM-1/10PRINT.asm
+++ machines/KIM-1/10PRINT.asm
@@ -1,154 +1,160 @@
-; 10PRINT for KIM-1 using scrolling display technique
-; by Michael Doornbos <mike@imapenguin.com> 2025 
+; 10PRINT FOR KIM-1 USING SCROLLING DISPLAY TECHNIQUE
+; BY MICHAEL DOORNBOS <MIKE@IMAPENGUIN.COM> 2025 
 
-; This program generates a random pattern of slashes and backslashes
-; and displays it on the KIM-1's 7-segment display.
+; THIS PROGRAM GENERATES A RANDOM PATTERN OF SLASHES AND BACKSLASHES
+; AND DISPLAYS IT ON THE KIM-1'S 7-SEGMENT DISPLAY.
 
-; The pattern scrolls to the left, creating a continuous effect.
+; THE PATTERN SCROLLS TO THE LEFT, CREATING A CONTINUOUS EFFECT.
 
+; CONSTANTS FOR 7-SEGMENT DISPLAY CHARACTERS
+BACKSLASH  .EQU $64        ; BACKSLASH CHARACTER
+SLASH      .EQU $52        ; FORWARD SLASH CHARACTER
+SPC        .EQU $80        ; SPACE CHARACTER
 
-; Constants for 7-segment display characters
-backslash  .equ $64        ; Backslash character
-slash      .equ $52        ; Forward slash character
-spc        .equ $80        ; Space character
+; KIM-1 HARDWARE ADDRESSES
+SAD     .EQU $1740      ; DATA PORT FOR PINS 1-4
+SADD    .EQU $1741      ; DATA DIRECTION REGISTER A
+SBD     .EQU $1742      ; DATA PORT FOR PINS 5-6
+SBDD    .EQU $1743      ; DATA DIRECTION REGISTER B
+TIMER2  .EQU $1747      ; OPTIONAL 2ND 6532 TIMER
+LOUT    .EQU $7F        ; SET PINS AS OUTPUT TO LEFT 4 LEDS
+ROUT    .EQU $1E        ; SET PINS AS OUTPUT TO RIGHT 2 LEDS
 
-; KIM-1 hardware addresses
-SAD     .equ $1740      ; data port for pins 1-4
-SADD    .equ $1741      ; data direction register A
-SBD     .equ $1742      ; data port for pins 5-6
-SBDD    .equ $1743      ; data direction register B
-TIMER2  .equ $1747      ; optional 2nd 6532 timer
-lout    .equ $7f        ; set pins as output to left 4 LEDs
-rout    .equ $1e        ; set pins as output to right 2 LEDs
+; ZERO PAGE VARIABLES
+SEED    .EQU $00D0      ; RANDOM SEED LOCATION
+TMR     .EQU $00DB      ; TIMER COUNTER
+PTR     .EQU $00DC      ; POINTER
+XFRHI   .EQU $00DD      ; USED FOR CHARACTER BUFFER HIGH BYTE
+XFRLO   .EQU $00DE      ; USED FOR CHARACTER BUFFER LOW BYTE
+TMP1    .EQU $00DF      ; TEMPORARY STORAGE
+CBUFF   .EQU $00E8      ; CHARACTER BUFFER (6 BYTES)
+MSGBUF  .EQU $0180      ; BUFFER FOR GENERATED PATTERNS (30 BYTES)
 
-; Zero page variables
-SEED    .equ $00D0      ; Random seed location
-tmr     .equ $00DB      ; Timer counter
-ptr     .equ $00DC      ; Pointer
-xfrhi   .equ $00DD      ; Used for character buffer high byte
-xfrlo   .equ $00DE      ; Used for character buffer low byte
-tmp1    .equ $00DF      ; Temporary storage
-cbuff   .equ $00E8      ; Character buffer (6 bytes)
-MSGBUF  .equ $0180      ; Buffer for generated patterns (30 bytes)
+        .ORG $0200      ; START OF PROGRAM CODE
 
-        .org $0200      ; Start of program code
-
-main    
-        ; Clear the message buffer first to prevent glitches
+MAIN    
+        ; CLEAR THE MESSAGE BUFFER FIRST TO PREVENT GLITCHES
         LDX #$00
-clrloop LDA #spc        ; Use space character to initialize
+CLRLOOP LDA #SPC        ; USE SPACE CHARACTER TO INITIALIZE
         STA MSGBUF,X
         INX
-        CPX #$30        ; Clear the entire buffer area
-        BNE clrloop
+        CPX #$30        ; CLEAR THE ENTIRE BUFFER AREA
+        BNE CLRLOOP
         
-        LDA #$00        ; Add null terminator at the end
+        LDA #$00        ; ADD NULL TERMINATOR AT THE END
         STA MSGBUF+23
         
-        LDA #$42        ; Initialize random seed
-        STA SEED        ; with non-zero value
+        ; INITIALIZE THE TIMER
+        LDA #$FF        ; LOAD MAXIMUM VALUE
+        STA TIMER2      ; START TIMER
         
-        JSR GENPAT      ; Generate initial pattern
+        ; USE TIMER VALUE AS SEED
+        LDA TIMER2      ; READ CURRENT TIMER VALUE
+        STA SEED        ; USE AS RANDOM SEED
+        BNE SEEDOK      ; IF NOT ZERO, IT'S FINE
+        INC SEED        ; OTHERWISE INCREMENT TO MAKE NON-ZERO
         
-infinit LDY #>MSGBUF    ; Load buffer location
+SEEDOK  JSR GENPAT      ; GENERATE INITIAL PATTERN
+        
+INFINIT LDY #>MSGBUF    ; LOAD BUFFER LOCATION
         LDA #<MSGBUF
-        JSR scan        ; Display the pattern
+        JSR SCAN        ; DISPLAY THE PATTERN
         
-        ; Generate new random slash at end of buffer
-        JSR RANDOM      ; Get random bit
-        BCC genback     ; Branch if carry clear (50% chance)
+        ; GENERATE NEW RANDOM SLASH AT END OF BUFFER
+        JSR RANDOM      ; GET RANDOM BIT
+        BCC GENBACK     ; BRANCH IF CARRY CLEAR (50% CHANCE)
         
-        LDA #slash      ; Forward slash
-        JMP store
+        LDA #SLASH      ; FORWARD SLASH
+        JMP STORE
         
-genback LDA #backslash  ; Backslash
+GENBACK LDA #BACKSLASH  ; BACKSLASH
         
-store   STA MSGBUF+22   ; Add new character to end of buffer
+STORE   STA MSGBUF+22   ; ADD NEW CHARACTER TO END OF BUFFER
         
-        ; Shift buffer left one position (scroll effect)
-        LDX #$00        ; Start at first position
-shift   LDA MSGBUF+1,X  ; Get next character
-        STA MSGBUF,X    ; Store in current position
-        INX             ; Move to next position
-        CPX #$22        ; Check if we're at end of buffer
-        BNE shift       ; Continue if not at end
+        ; SHIFT BUFFER LEFT ONE POSITION (SCROLL EFFECT)
+        LDX #$00        ; START AT FIRST POSITION
+SHIFT   LDA MSGBUF+1,X  ; GET NEXT CHARACTER
+        STA MSGBUF,X    ; STORE IN CURRENT POSITION
+        INX             ; MOVE TO NEXT POSITION
+        CPX #$22        ; CHECK IF WE'RE AT END OF BUFFER
+        BNE SHIFT       ; CONTINUE IF NOT AT END
         
-        ; Ensure null terminator is always present
+        ; ENSURE NULL TERMINATOR IS ALWAYS PRESENT
         LDA #$00
         STA MSGBUF+23
         
-        JMP infinit     ; Loop forever
+        JMP INFINIT     ; LOOP FOREVER
 
-; Generate initial pattern buffer with random slashes
-GENPAT  LDX #$00        ; Start at first position
-gploop  JSR RANDOM      ; Get random bit
-        BCC gback       ; Branch if carry clear
+; GENERATE INITIAL PATTERN BUFFER WITH RANDOM SLASHES
+GENPAT  LDX #$00        ; START AT FIRST POSITION
+GPLOOP  JSR RANDOM      ; GET RANDOM BIT
+        BCC GBACK       ; BRANCH IF CARRY CLEAR
         
-        LDA #slash      ; Forward slash
-        JMP gstore
+        LDA #SLASH      ; FORWARD SLASH
+        JMP GSTORE
         
-gback   LDA #backslash  ; Backslash
+GBACK   LDA #BACKSLASH  ; BACKSLASH
         
-gstore  STA MSGBUF,X    ; Store in buffer
-        INX             ; Next position
-        CPX #$17        ; Check if buffer is full
-        BNE gploop      ; Continue if not full
+GSTORE  STA MSGBUF,X    ; STORE IN BUFFER
+        INX             ; NEXT POSITION
+        CPX #$17        ; CHECK IF BUFFER IS FULL
+        BNE GPLOOP      ; CONTINUE IF NOT FULL
         
-        LDA #$00        ; Add null terminator
-        STA MSGBUF+23   ; at end of buffer
-        RTS             ; Return
+        LDA #$00        ; ADD NULL TERMINATOR
+        STA MSGBUF+23   ; AT END OF BUFFER
+        RTS             ; RETURN
 
-; Random number generator (8-bit LFSR)
-RANDOM  LDA SEED        ; Load current seed
-        ASL             ; Shift left (C gets high bit)
-        BCC NOEOR       ; Skip EOR if bit 7 was 0
-        EOR #$B4        ; Apply feedback polynomial
-NOEOR   STA SEED        ; Store updated seed
-        RTS             ; Return with carry = random bit
+; RANDOM NUMBER GENERATOR (8-BIT LFSR)
+RANDOM  LDA SEED        ; LOAD CURRENT SEED
+        ASL             ; SHIFT LEFT (C GETS HIGH BIT)
+        BCC NOEOR       ; SKIP EOR IF BIT 7 WAS 0
+        EOR #$B4        ; APPLY FEEDBACK POLYNOMIAL
+NOEOR   STA SEED        ; STORE UPDATED SEED
+        RTS             ; RETURN WITH CARRY = RANDOM BIT
 
-; Scanning routine from original code
-scan    STY xfrlo       ; y and a get loaded before jsr to scan
-        STA xfrhi
-        LDA #$07        ; init scan forward
-        STA tmp1
-        LDY #$05        ; init y
-cont    LDX #$05        ; init x
-char    LDA (xfrhi),Y   ; get character
-        CMP #$00        ; last character?
-        BNE more        ; if not, continue
+; SCANNING ROUTINE FROM ORIGINAL CODE
+SCAN    STY XFRLO       ; Y AND A GET LOADED BEFORE JSR TO SCAN
+        STA XFRHI
+        LDA #$07        ; INIT SCAN FORWARD
+        STA TMP1
+        LDY #$05        ; INIT Y
+CONT    LDX #$05        ; INIT X
+CHAR    LDA (XFRHI),Y   ; GET CHARACTER
+        CMP #$00        ; LAST CHARACTER?
+        BNE MORE        ; IF NOT, CONTINUE
         RTS
-more    STA cbuff,X     ; store char
-        DEY             ; set up next char
-        DEX             ; set up next store loc
-        BPL char        ; loop if not 6th char
-        CLD             ; binary mode
-        CLC             ; prepare to add (clear carry flag)
-        TYA             ; get char pointer
-        ADC tmp1        ; update for 6 new characters
-        STA ptr         ; save new pointer
-        JSR dspdly      ; delay display
-        LDY ptr         ; restore pointer
-        JMP cont        ; continue with rest of message
+MORE    STA CBUFF,X     ; STORE CHAR
+        DEY             ; SET UP NEXT CHAR
+        DEX             ; SET UP NEXT STORE LOC
+        BPL CHAR        ; LOOP IF NOT 6TH CHAR
+        CLD             ; BINARY MODE
+        CLC             ; PREPARE TO ADD (CLEAR CARRY FLAG)
+        TYA             ; GET CHAR POINTER
+        ADC TMP1        ; UPDATE FOR 6 NEW CHARACTERS
+        STA PTR         ; SAVE NEW POINTER
+        JSR DSPDLY      ; DELAY DISPLAY
+        LDY PTR         ; RESTORE POINTER
+        JMP CONT        ; CONTINUE WITH REST OF MESSAGE
 
-dspdly  LDX #$0A        ; set the delay rate here
-        STX tmr         ; put in decr. location
-time    LDA #$52        ; load timer
-        STA TIMER2      ; start timer
-lite    JSR disp        ; gosub display rtn
-        BIT TIMER2      ; timer done?
-        BPL lite        ; if not, loop
-        DEC tmr         ; decrement timer counter
-        BNE time        ; not finished
-        RTS             ; now get 6 new characters
+DSPDLY  LDX #$0A        ; SET THE DELAY RATE HERE
+        STX TMR         ; PUT IN DECR. LOCATION
+TIME    LDA #$52        ; LOAD TIMER
+        STA TIMER2      ; START TIMER
+LITE    JSR DISP        ; GOSUB DISPLAY RTN
+        BIT TIMER2      ; TIMER DONE?
+        BPL LITE        ; IF NOT, LOOP
+        DEC TMR         ; DECREMENT TIMER COUNTER
+        BNE TIME        ; NOT FINISHED
+        RTS             ; NOW GET 6 NEW CHARACTERS
 
-disp    LDA #lout       ; change left led segments
-        STA SADD        ; to outputs
-        LDY #$00        ; init recall index
-        LDX #$09        ; init digit number
-six     LDA cbuff,Y     ; get character
-        STY $00FC       ; save y for monitor disp routine
-        JSR $1F4E       ; monitor routine - disp char, delay 500 cycles
-        INY             ; set up for next char
-        CPY #$06        ; 6 char displayed?
-        BCC six         ; no
+DISP    LDA #LOUT       ; CHANGE LEFT LED SEGMENTS
+        STA SADD        ; TO OUTPUTS
+        LDY #$00        ; INIT RECALL INDEX
+        LDX #$09        ; INIT DIGIT NUMBER
+SIX     LDA CBUFF,Y     ; GET CHARACTER
+        STY $00FC       ; SAVE Y FOR MONITOR DISP ROUTINE
+        JSR $1F4E       ; MONITOR ROUTINE - DISP CHAR, DELAY 500 CYCLES
+        INY             ; SET UP FOR NEXT CHAR
+        CPY #$06        ; 6 CHAR DISPLAYED?
+        BCC SIX         ; NO
         RTS
